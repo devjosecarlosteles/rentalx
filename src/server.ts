bec766271f5bc6ea75b21a4c6e28c4e676088a30
devjosecarlosteles <devjosecarlosteles@gmail.com>
2021-09-18 23:09:54 -0300

    ts-node-dev config and auto-reload

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from 'express'
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.json({ message: "Hello World" })
+})
+
 app.listen(3333, () => console.log('Sever is running!'))
