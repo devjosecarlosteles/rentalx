03caddea9cc92773e8b5cf004da66420e4b1047a
devjosecarlosteles <devjosecarlosteles@gmail.com>
2021-09-30 22:06:40 -0300

    use categoriesRoutes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import { categoriesRoutes } from './routes/categories.routes'
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.json({ message: "Hello World" })
 })
+
+app.use(categoriesRoutes)
 
 app.listen(3333, () => console.log('Sever is running!'))
