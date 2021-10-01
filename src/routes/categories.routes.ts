f0d62267f78d011e2f2a09b7fdd630941db9218c
devjosecarlosteles <devjosecarlosteles@gmail.com>
2021-09-30 22:05:34 -0300

    categories params

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,8 +2,17 @@ import { Router } from "express"
 
 const categoriesRoutes = Router()
 
+const categories = []
+
 categoriesRoutes.post("/categories", (req, res) => {
   const { name, description } = req.body
+
+  categories.push({
+    name,
+    description
+  })
+
+  res.status(201)
 })
 
 export { categoriesRoutes }
